61a24184943cb390b001a7387d8be65940d635a5
Richard Brown <flukefan@googlemail.com>
2012-01-27 11:15:29 +0000

    Updated to use location.href

--- Source/OfflineExample/Scripts/Offline/OfflineGlobal.js
+++ Source/OfflineExample/Scripts/Offline/OfflineGlobal.js
@@ -43,7 +43,7 @@ OfflineGlobal.prototype.updatedCache = function () {
     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
         // manifest was updated, so refresh the page with the new cache content
         window.applicationCache.swapCache();
-        document.location.href = window.location;
+        document.location.href = window.location.href;
     }
 }
 
